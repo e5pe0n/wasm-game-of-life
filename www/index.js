6c73c8a4ece407e69f040cc8686c25d4c6e5ad66
e5pe0n <1izard.synchr0fi5h@gmail.com>
2024-02-18 20:48:23 +0900

    toggle

--- www/index.js
+++ www/index.js
@@ -1,6 +1,8 @@
 import { Universe, Cell } from "wasm-game-of-life";
 import { memory } from "wasm-game-of-life/wasm_game_of_life_bg.wasm";
 
+let animatedId = null;
+
 const CELL_SIZE = 5; // px
 const GRID_COLOR = "#CCCCCC";
 const DEAD_COLOR = "#FFFFFF";
@@ -13,6 +15,23 @@ const height = universe.height();
 const canvas = document.getElementById("game-of-life-canvas");
 canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
+canvas.onclick = (e) => {
+  const boundingRect = canvas.getBoundingClientRect();
+
+  const scaleX = canvas.width / boundingRect.width;
+  const scaleY = canvas.height / boundingRect.height;
+
+  const canvasLeft = (e.clientX - boundingRect.left) * scaleX;
+  const canvasTop = (e.clientY - boundingRect.top) * scaleY;
+
+  const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+  const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+
+  universe.toggle_cell(row, col);
+
+  drawGrid();
+  drawCells();
+};
 
 const ctx = canvas.getContext("2d");
 
@@ -66,7 +85,29 @@ const renderLoop = () => {
   drawGrid();
   drawCells();
 
-  requestAnimationFrame(renderLoop);
+  animatedId = requestAnimationFrame(renderLoop);
+};
+
+const isPaused = () => {
+  return animatedId === null;
+};
+
+const playPauseButton = document.getElementById("play-pause");
+const play = () => {
+  playPauseButton.textContent = "⏸";
+  renderLoop();
+};
+const pause = () => {
+  playPauseButton.textContent = "▶";
+  cancelAnimationFrame(animatedId);
+  animatedId = null;
+};
+playPauseButton.onclick = () => {
+  if (isPaused()) {
+    play();
+  } else {
+    pause();
+  }
 };
 
 drawGrid();
